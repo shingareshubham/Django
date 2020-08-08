62952e5d69543a5ac368455e2dea5701fa27303d
Shubham Shingare <shubham@Shubhams-MacBook-Air.local>
2020-08-08 10:42:40 -0700

    Postgresql DB connection

--- django_restfull/django_restfull/settings.py
+++ django_restfull/django_restfull/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'django_restfull.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'postgres',
+        'PASSWORD': 'Balaji@276',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
